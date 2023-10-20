6606a632dce3de6370d6b876593bbdc4a7c4cc57
33-66 <simon.nganga@student.moringaschool.com>
2023-10-20 15:24:47 +0300

    updated work

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+        const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    
+    return updatedEmployee;
+  }
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
+  console.log(updatedEmployee);
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    
+    employee[key] = value
+
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) { 
+    const newEmployee = { ...employee };
+   delete newEmployee[key];
+    return newEmployee;
+}
+const employee = {
+    name: 'Sam',
+    streetAddress: '12 Broadway'
+};
+const newEmployee = deleteFromEmployeeByKey(employee, 'name');
+console.log(newEmployee);
+console.log(employee);
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    delete employee[key];
+  
+    return employee;
+}
+
+
+
+destructivelyDeleteFromEmployeeByKey(employee, 'name');
+
+
+console.log(employee);
